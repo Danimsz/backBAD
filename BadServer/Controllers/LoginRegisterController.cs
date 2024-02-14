6700721d921a601284cc2326ffadc7d65137b038
aloprio <93702741+aloprio@users.noreply.github.com>
2024-02-14 19:36:26 +0100

    Cambios en el codigo para que funcione la cesta y para arreglar login
    Hay que cambiar lo de la cesta

--- BadServer/Controllers/LoginRegisterController.cs
+++ BadServer/Controllers/LoginRegisterController.cs
@@ -36,6 +36,7 @@ namespace BadServer.Controllers
 
             // Busca un usuario que coincida con el nombre de usuario y la contraseña hasheada
             var user = await _dbContext.Clientes.FirstOrDefaultAsync(u => u.UserName == loginDto.UserName && u.Password == hashedPassword);
+              
 
             // Control por si el usuario y la contraseña no coinciden
             if (user == null)
@@ -43,16 +44,17 @@ namespace BadServer.Controllers
                 return Unauthorized("Usuario o contraseña incorrectos");
             }
 
-            //Asociar una cesta al usuario si no tiene una
+            // Asociar una cesta al usuario si no tiene una
             if (user.Cesta == null)
             {
                 user.Cesta = new Cesta();
                 await _dbContext.SaveChangesAsync();
 
-                if(user.Cesta.CestaID > 0)
+                if (user.Cesta.CestaID > 0)
                 {
                     Console.WriteLine("La cesta se ha creado");
-                } else
+                }
+                else
                 {
                     Console.WriteLine("La cesta NO se ha creado");
                 }
@@ -60,13 +62,13 @@ namespace BadServer.Controllers
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                //Aqui añadimos los datos que sirvan para autorizrr al usuario
+                //Aqui añadimos los datos que sirvan para autorizar al usuario
                 Claims = new Dictionary<string, object>
-                {
-                    { "id", Guid.NewGuid().ToString() },
-                    { "CestaId", user.Cesta.CestaID.ToString() },
-
-                },
+            {
+                { "id", Guid.NewGuid().ToString() },
+                { "CestaId", user.Cesta.CestaID.ToString() },
+                { "UserId", user.ClienteID } // Agregamos el UserId al token
+            },
                 //Aqui indicamos cuando caduca el token
                 Expires = DateTime.UtcNow.AddDays(365),
                 //Aqui especificamos nuestra clave y el algoritmo de firmado
@@ -75,11 +77,12 @@ namespace BadServer.Controllers
             };
 
             //Creamos el token y se lo devolvemos al usuario logueado
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();   
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
             SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
             string stringToken = tokenHandler.WriteToken(token);
 
-            return Ok(stringToken);//el token
+            // Devolvemos tanto el token como el UserId
+            return Ok(new { Token = stringToken, UserId = user.ClienteID });
         }
 
 
